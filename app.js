f911aa56fe7e022b9c22090fb27a2c05149d188a
mattfan00 <kungfumo@gmail.com>
2020-05-23 23:47:49 -0400

    initialized server

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express = require('express')
       app = express()
+      config = require('config')
+      mongoose = require('mongoose')
+      passport = require('passport')
+      LocalStrategy = require('passport-local')
+      User = require('./models/user')
+
+const userRoutes = require('./routes/user')
 
 
+
+const db = config.get('mongoURI')
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.static(__dirname + "/public"))
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.use(require("express-session")({
+  secret: "this is my secret",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+
+
+app.use(userRoutes)
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -13,5 +42,9 @@ app.get('/', (req, res) => {
 app.get('/game', (req, res) => {
   res.render('game')
 })
+
+
+
+
 
 app.listen(3000, () => console.log('Tetris server running'))
